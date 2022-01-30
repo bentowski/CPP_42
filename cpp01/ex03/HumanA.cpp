e59dbf56899afe7f2f8d3df7479837a1ed46c9f9
bentowski <benjamin.baudry@hotmail.com>
2022-01-30 15:59:47 +0100

    cpp01 fini, avec header, verif a faire

--- cpp01/ex03/HumanA.cpp
+++ cpp01/ex03/HumanA.cpp
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   HumanA.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bbaudry <bbaudry@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/30 15:58:19 by bbaudry           #+#    #+#             */
+/*   Updated: 2022/01/30 15:58:20 by bbaudry          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "HumanA.hpp"
 
 HumanA::HumanA( std::string newName, Weapon& newWeapon ) : name(newName), weapon(newWeapon)
